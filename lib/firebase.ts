133dc4f393b005b5f1b0057fbaf761cae61ca05b
Luca04B <luca.baumann04@gmx.de>
2025-11-14 16:52:47 +0100

    Test

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyBCqmHqmZwveNImkInDpQh4eXVowwIZEpE",
+  authDomain: "trackyourprogress-23b12.firebaseapp.com",
+  projectId: "trackyourprogress-23b12",
+  storageBucket: "trackyourprogress-23b12.firebasestorage.app",
+  messagingSenderId: "593638477178",
+  appId: "1:593638477178:web:c1ac4fb217eb7657021ac3",
 };
 
 const app = initializeApp(firebaseConfig);
